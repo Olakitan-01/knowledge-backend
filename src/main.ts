2dfb7651596c735c94fd37c27565a11b9d70b7b0
Olanrewaju Emmanuel <olanrewajuemma2023@gmail.com>
2026-03-16 14:19:10 +0100

    fix: updated_base_url

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://knowledge-frontend-00ld.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
